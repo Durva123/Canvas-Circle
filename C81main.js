5345eadf16bb0633d801751e86df42192e629216
Durva123 <84019341+Durva123@users.noreply.github.com>
2021-06-21 16:13:55 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,25 @@
+   var canvas = document.getElementById("mycanvas");
+   ctx=canvas.getContext("2d");
+   color="red";
+   ctx.beginPath();
+   ctx.strokeStyle=color;
+   ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e){
+mouse_x=e.clientX-canvas.offsetLeft;
+mouse_Y=e.clientY-canvas.offsetTop;
+console.log("X="+ mouse_x+",Y=  "+mouse_Y);
+circle(mouse_x, mouse_Y);
+} 
+function circle(mouse_x, mouse_Y){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
+    ctx.stroke();  
+}
+function cleararea(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
